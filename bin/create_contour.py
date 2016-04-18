1736883e6635a13aa896209e3649c9b30b87b54d
Bart Römgens <bart.romgens@gmail.com>
2016-04-18 15:06:08 +0200

    Create contour data for multiple climate data types

--- bin/create_contour.py
+++ bin/create_contour.py
@@ -8,17 +8,23 @@ sys.path.append('./climatemaps')
 import climatemaps
 
 
-DATA_DIR = './website/data'
+DATA_OUT_DIR = './website/data'
+
+TYPES = {
+    'precipitation': './data/precipitation/cpre6190.dat',
+    'cloud': './data/cloud/ccld6190.dat',
+}
 
 
-def test():
-    for month in range(1, 13):
-        latrange, lonrange, Z = climatemaps.data.import_climate_data(month)
-        filepath_out = os.path.join(DATA_DIR, 'contour_cloud_' + str(month) + '.json')
-        test_config = climatemaps.contour.ContourPlotConfig()
-        contourmap = climatemaps.contour.Contour(test_config, lonrange, latrange, Z)
-        contourmap.create_contour_data(filepath_out)
+def main():
+    for data_type, filepath in TYPES.items():
+        for month in range(1, 13):
+            latrange, lonrange, Z = climatemaps.data.import_climate_data(filepath, month)
+            filepath_out = os.path.join(DATA_OUT_DIR, 'contour_' + data_type +'_' + str(month) + '.json')
+            test_config = climatemaps.contour.ContourPlotConfig()
+            contourmap = climatemaps.contour.Contour(test_config, lonrange, latrange, Z)
+            contourmap.create_contour_data(filepath_out)
 
 
 if __name__ == "__main__":
-    test()
+    main()
