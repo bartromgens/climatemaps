5d9b335ba22038410438411a306df437fce3f99d
Bart Römgens <bart.romgens@gmail.com>
2016-04-19 19:05:05 +0200

    Re-enable normal contour line to json function

--- climatemaps/contour.py
+++ climatemaps/contour.py
@@ -27,7 +27,7 @@ def angle(v1, v2):
 
 class ContourPlotConfig(object):
     def __init__(self, level_lower=0, level_upper=100, colormap=plt.cm.jet, unit=''):  # jet, jet_r, YlOrRd, gist_rainbow
-        self.n_contours = 160
+        self.n_contours = 16
         self.min_angle_between_segments = 15
         self.level_lower = level_lower
         self.level_upper = level_upper
@@ -40,6 +40,7 @@ class Contour(object):
         self.config = config
         self.Z = Z
         self.lonrange = lonrange
+        # self.latrange = latrange
         self.latrange = latrange[::-1]
         numpy.set_printoptions(3, threshold=100, suppress=True)  # .3f
 
@@ -49,29 +50,36 @@ class Contour(object):
         m = Basemap(
             projection='merc',
             resolution='c',
-            lon_0=360,
+            lon_0=-180,
             ax=ax,
-            llcrnrlon=-180,
+            llcrnrlon=0,
             llcrnrlat=-85,
-            urcrnrlon=180,
+            urcrnrlon=360,
             urcrnrlat=85,
         )
         x, y = m(*numpy.meshgrid(self.lonrange, self.latrange))
         # levels = numpy.linspace(10, 90, num=self.config.n_contours)
-        levels = numpy.linspace(self.config.level_lower, self.config.level_upper, num=self.config.n_contours)
+        levels = numpy.linspace(self.config.level_lower, self.config.level_upper, num=self.config.n_contours*10)
         # contours = plt.contourf(lonrange, latrange, Z, levels=levels, cmap=plt.cm.plasma)
         m.contourf(x, y, self.Z, levels=levels, cmap=self.config.colormap)
-        m.drawcoastlines(linewidth=0.2)  # draw coastlines
+        m.drawcoastlines(linewidth=0.5)  # draw coastlines
         # m.drawmapboundary()  # draw a line around the map region
         # m.drawparallels(numpy.arange(-90., 120., 30.), labels=[1, 0, 0, 0])  # draw parallels
         # m.drawmeridians(numpy.arange(0., 420., 60.), labels=[0, 0, 0, 1])  # draw meridians
         # cbar = figure.colorbar(contours, format='%.1f')
         # ax.set_xlim([0, 360e5])
         ax.set_axis_off()
-        plt.savefig(filepath + '.png', dpi=700, bbox_inches='tight', pad_inches=0)
-        # contours = plt.contourf(self.lonrange, self.latrange, self.Z, levels=levels, cmap=self.config.colormap)
+        plt.savefig(filepath + '.png', dpi=200, bbox_inches='tight', pad_inches=0)
+
+        self.create_contour_json(filepath)
+
+    def create_contour_json(self, filepath):
+        figure = plt.figure()
+        ax = figure.add_subplot(111)
+        levels = numpy.linspace(self.config.level_lower, self.config.level_upper, num=self.config.n_contours)
+        contours = ax.contour(self.lonrange, self.latrange, self.Z, levels=levels, cmap=self.config.colormap)
         ndigits = 3
-        # contour_to_json(contours, filepath, levels, self.config.min_angle_between_segments, ndigits, self.config.unit)
+        contour_to_json(contours, filepath, levels, self.config.min_angle_between_segments, ndigits, self.config.unit)
 
 
 def contour_to_json(contour, filename, contour_labels, min_angle=2, ndigits=5, unit=''):
@@ -99,6 +107,7 @@ def contour_to_json(contour, filename, contour_labels, min_angle=2, ndigits=5, u
                 for i in range(1, len(v) - 2):
                     v2 = v[i + 1] - v[i - 1]
                     diff_angle = math.fabs(angle(v1, v2) * 180.0 / math.pi)
+                    # print(diff_angle)
                     if diff_angle > min_angle:
                         x.append(round(v[i][0], ndigits))
                         y.append(round(v[i][1], ndigits))
