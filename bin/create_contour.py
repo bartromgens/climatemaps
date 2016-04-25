fb4dd25c5e347aa421c1329805d57dcbe90ace66
Bart Römgens <bart.romgens@gmail.com>
2016-04-25 15:11:51 +0200

    Create optimal map that combines data
    Optimal is a function of input data and some
    subjectively set optimum functions for eacht data type.

--- bin/create_contour.py
+++ bin/create_contour.py
@@ -2,7 +2,9 @@
 
 import sys
 import os
+import math
 
+import numpy
 import matplotlib.pyplot as plt
 
 sys.path.append('./climatemaps')
@@ -52,12 +54,49 @@ TYPES = {
 
 
 def main():
-    for data_type, settings in TYPES.items():
-        for month in range(1, 13):
-            latrange, lonrange, Z = climatemaps.data.import_climate_data(settings['filepath'], month, settings['conversion_factor'])
-            filepath_out = os.path.join(DATA_OUT_DIR, 'contour_' + data_type +'_' + str(month))
-            contourmap = climatemaps.contour.Contour(settings['config'], lonrange, latrange, Z)
-            contourmap.create_contour_data(filepath_out)
+    # for data_type, settings in TYPES.items():
+    #     for month in range(1, 13):
+    #         latrange, lonrange, Z = climatemaps.data.import_climate_data(settings['filepath'], month, settings['conversion_factor'])
+    #         filepath_out = os.path.join(DATA_OUT_DIR, 'contour_' + data_type +'_' + str(month))
+    #         contourmap = climatemaps.contour.Contour(settings['config'], lonrange, latrange, Z)
+    #         contourmap.create_contour_data(filepath_out)
+    for month in range(1, 13):
+        create_optimal_map(month)
+
+
+def create_optimal_map(month):
+    settings = TYPES['precipitation']
+    latrange, lonrange, Zpre = climatemaps.data.import_climate_data(settings['filepath'], month, settings['conversion_factor'])
+
+    settings = TYPES['cloud']
+    latrange, lonrange, Zcloud = climatemaps.data.import_climate_data(settings['filepath'], month, settings['conversion_factor'])
+
+    settings = TYPES['maxtemp']
+    latrange, lonrange, Ztmax = climatemaps.data.import_climate_data(settings['filepath'], month, settings['conversion_factor'])
+
+    for x in numpy.nditer(Zpre, op_flags=['readwrite']):
+        if x/16.0 > 1.0:
+            x[...] = 0.0
+        else:
+            x[...] = 1.0 - x/16.0
+
+    for x in numpy.nditer(Ztmax, op_flags=['readwrite']):
+        diff_from_best = math.pow(abs(x - 20.0)/10.0, 2)
+        x[...] = 1.0 - diff_from_best
+
+    factor = 0.33
+    Z = ((Zpre + (100 - Zcloud)/100 + Ztmax) * factor) * 10.0
+
+    for x in numpy.nditer(Z, op_flags=['readwrite']):
+        x[...] = max(x, 0.0)
+
+    # Z = Ztmax
+
+    filepath_out = os.path.join(DATA_OUT_DIR, 'contour_' + 'optimal' +'_' + str(month))
+    conofig = climatemaps.contour.ContourPlotConfig(0.0, 10.0, colormap=plt.cm.RdYlGn, unit='')
+    contourmap = climatemaps.contour.Contour(conofig, lonrange, latrange, Z)
+    contourmap.create_contour_data(filepath_out)
+    print('month done: '  + str(month))
 
 
 if __name__ == "__main__":
