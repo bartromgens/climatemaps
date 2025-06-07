44ecd8188ede5ae7bcae23a627b4a64f8ddd4e4f
Bart Römgens <bart.romgens@gmail.com>
2025-06-07 13:54:13 +0200

    Refactor: extract method

--- scripts/create_contour.py
+++ scripts/create_contour.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
 
+from climatemaps.datasets import ContourConfig
+
 sys.path.append('../climatemaps')
 
 import climatemaps
@@ -19,7 +21,6 @@ CREATE_IMAGES = True
 
 
 CONTOUR_TYPES = climatemaps.datasets.CLIMATE_MODEL_DATA_SETS + climatemaps.datasets.HISTORIC_DATA_SETS
-
 CONTOUR_TYPES = list(filter(lambda x: x.data_type == 'model_precipitation_5m_ssp585_2081_2100', CONTOUR_TYPES))
 
 
@@ -30,29 +31,33 @@ def main():
     for config in CONTOUR_TYPES:
         for month in range(1, month_upper+1):
             logger.info('create image and tiles for "' + config.data_type + '" and month ' + str(month))
-            progress = counter/n_data_sets*100.0
+            progress = counter/n_data_sets * 100.0
             logger.info(f"progress: {(int(progress))} %")
-            latrange, lonrange, Z = None, None, None
-            if config.format == climatemaps.datasets.DataFormat.IPCC_GRID:
-                latrange, lonrange, Z = climatemaps.data.import_climate_data(config.filepath, month)
-            elif config.format == climatemaps.datasets.DataFormat.GEOTIFF:
-                lonrange, latrange, Z = climatemaps.geotiff.read_geotiff_month(config.filepath, month-1)
-                latrange = latrange*-1
-            else:
-                assert f'DataFormat {config.format} is not supported'
-            Z = Z * config.conversion_factor
-            # logger.info(f"lon: {lonrange}")
-            # logger.info(f"lat: {latrange}")
-            contourmap = climatemaps.contour.Contour(config.config, lonrange, latrange, Z, zoom_min=ZOOM_MIN, zoom_max=ZOOM_MAX)
-            contourmap.create_contour_data(
-                DATA_OUT_DIR,
-                config.data_type,
-                month,
-                figure_dpi=1200,
-                create_images=CREATE_IMAGES,
-                zoomfactor=ZOOM_FACTOR
-            )
+            _create_contour(config, month)
             counter += 1
+
+
+def _create_contour(config: ContourConfig, month: int):
+    lat_range, lon_range, values = None, None, None
+    if config.format == climatemaps.datasets.DataFormat.IPCC_GRID:
+        lat_range, lon_range, values = climatemaps.data.import_climate_data(config.filepath, month)
+    elif config.format == climatemaps.datasets.DataFormat.GEOTIFF:
+        lon_range, lat_range, values = climatemaps.geotiff.read_geotiff_month(config.filepath, month - 1)
+        lat_range = lat_range * -1
+    else:
+        assert f'DataFormat {config.format} is not supported'
+    values = values * config.conversion_factor
+    contour_map = climatemaps.contour.Contour(
+        config.config, lon_range, lat_range, values, zoom_min=ZOOM_MIN, zoom_max=ZOOM_MAX
+    )
+    contour_map.create_contour_data(
+        DATA_OUT_DIR,
+        config.data_type,
+        month,
+        figure_dpi=1200,
+        create_images=CREATE_IMAGES,
+        zoomfactor=ZOOM_FACTOR
+    )
 
 
 if __name__ == "__main__":
