d26d5b9cbfdd2e3cde84660613fe195a3770bab6
Bart Römgens <bart.romgens@gmail.com>
2025-07-25 22:31:18 +0200

    Update create config script after type changes

--- tileserver/create_config.py
+++ tileserver/create_config.py
@@ -3,27 +3,28 @@ import os
 import sys
 from typing import List
 
+
 module_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if module_dir not in sys.path:
     sys.path.insert(0, module_dir)
 
-from climatemaps.config import ClimateMap
+from climatemaps.datasets import ClimateDataConfig
 from climatemaps.settings import settings
 
 
 config = {"options": {"paths": {"root": "../data", "mbtiles": "tiles"}}, "data": {}}
 
 
-def main(data_sets: List[ClimateMap]):
-    for data_set in data_sets:
+def main(data_configs: List[ClimateDataConfig]):
+    for data_config in data_configs:
         for month in range(1, 13):
             # vector entry
-            config["data"][f"{data_set.data_type}_vector_{month}"] = {
-                "mbtiles": f"{data_set.data_type}/{month}_vector.mbtiles"
+            config["data"][f"{data_config.data_type_slug}_vector_{month}"] = {
+                "mbtiles": f"{data_config.data_type_slug}/{month}_vector.mbtiles"
             }
             # raster entry
-            config["data"][f"{data_set.data_type}_raster_{month}"] = {
-                "mbtiles": f"{data_set.data_type}/{month}_raster.mbtiles"
+            config["data"][f"{data_config.data_type_slug}_raster_{month}"] = {
+                "mbtiles": f"{data_config.data_type_slug}/{month}_raster.mbtiles"
             }
 
     # write to file
