b0c9d56a031f7d89b40485d2c074b0296c834745
Bart Römgens <bart.romgens@gmail.com>
2025-10-19 21:04:24 +0200

    Update matomo site id

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://openclimatemap.org/api/v1',
   matomoUrl: 'https://piwik.romgens.com/',
-  matomoSiteId: 9,
+  matomoSiteId: 2,
 };
