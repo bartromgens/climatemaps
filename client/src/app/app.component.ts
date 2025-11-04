b8ea0bd2259601d23c6be4c39148a29ff354e0ad
Bart Römgens <bart.romgens@gmail.com>
2025-11-04 19:29:50 +0100

    Refactor: search functionality into separate LocationSearchComponent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,15 @@
-import { Component, OnInit, HostListener, inject } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
-import { MatomoTracker } from 'ngx-matomo-client';
 
-import { GeocodingService, LocationSuggestion } from './core/geocoding.service';
-import { MapNavigationService } from './core/map-navigation.service';
 import { TemperatureUnitSelectorComponent } from './core/temperature-unit-selector.component';
 import { PrecipitationUnitSelectorComponent } from './core/precipitation-unit-selector.component';
+import { LocationSearchComponent } from './core/location-search.component';
 
 @Component({
   selector: 'app-root',
@@ -28,14 +20,11 @@ import { PrecipitationUnitSelectorComponent } from './core/precipitation-unit-se
     RouterLink,
     MatToolbarModule,
     MatButtonModule,
-    MatAutocompleteModule,
-    MatInputModule,
-    MatFormFieldModule,
     MatIconModule,
     MatMenuModule,
-    ReactiveFormsModule,
     TemperatureUnitSelectorComponent,
     PrecipitationUnitSelectorComponent,
+    LocationSearchComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -43,28 +32,14 @@ import { PrecipitationUnitSelectorComponent } from './core/precipitation-unit-se
 export class AppComponent implements OnInit {
   readonly title: string = 'OpenClimateMap';
   isMobile = false;
-  searchControl = new FormControl('');
-  filteredLocations$: Observable<LocationSuggestion[]>;
-  private readonly tracker = inject(MatomoTracker);
 
   constructor(
-    private geocodingService: GeocodingService,
-    private mapNavigationService: MapNavigationService,
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer,
   ) {
     this.matIconRegistry.addSvgIcon(
       'github',
       this.domSanitizer.bypassSecurityTrustResourceUrl('assets/github.svg'),
-    );
-
-    this.filteredLocations$ = this.searchControl.valueChanges.pipe(
-      debounceTime(300),
-      distinctUntilChanged(),
-      switchMap((value) => {
-        const query = typeof value === 'string' ? value : '';
-        return this.geocodingService.searchLocations(query);
-      }),
     );
   }
 
@@ -79,29 +54,5 @@ export class AppComponent implements OnInit {
 
   private checkMobile(): void {
     this.isMobile = window.innerWidth <= 768;
-  }
-
-  displayLocationName(location: LocationSuggestion | null): string {
-    return location ? location.displayName : '';
-  }
-
-  onLocationSelected(location: LocationSuggestion): void {
-    const zoom = this.geocodingService.getZoomLevelForLocation(location);
-    this.mapNavigationService.navigateToLocation(
-      location.lat,
-      location.lon,
-      zoom,
-      !this.isMobile,
-    );
-
-    this.tracker.trackEvent(
-      'Search',
-      'Location Selected',
-      location.displayName,
-    );
-  }
-
-  clearSearch(): void {
-    this.searchControl.setValue('');
   }
 }
---
+++ client/src/app/core/location-search.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { MatomoTracker } from 'ngx-matomo-client';
+
+import { GeocodingService, LocationSuggestion } from './geocoding.service';
+import { MapNavigationService } from './map-navigation.service';
+
+@Component({
+  selector: 'app-location-search',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './location-search.component.html',
+  styleUrl: './location-search.component.scss',
+})
+export class LocationSearchComponent implements OnInit {
+  searchControl = new FormControl('');
+  filteredLocations$: Observable<LocationSuggestion[]>;
+  private readonly tracker = inject(MatomoTracker);
+
+  constructor(
+    private geocodingService: GeocodingService,
+    private mapNavigationService: MapNavigationService,
+  ) {
+    this.filteredLocations$ = this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((value) => {
+        const query = typeof value === 'string' ? value : '';
+        return this.geocodingService.searchLocations(query);
+      }),
+    );
+  }
+
+  ngOnInit(): void {}
+
+  displayLocationName(location: LocationSuggestion | null): string {
+    return location ? location.displayName : '';
+  }
+
+  onLocationSelected(location: LocationSuggestion): void {
+    const zoom = this.geocodingService.getZoomLevelForLocation(location);
+    this.mapNavigationService.navigateToLocation(
+      location.lat,
+      location.lon,
+      zoom,
+      true,
+    );
+
+    this.tracker.trackEvent(
+      'Search',
+      'Location Selected',
+      location.displayName,
+    );
+  }
+
+  clearSearch(): void {
+    this.searchControl.setValue('');
+  }
+}
+
