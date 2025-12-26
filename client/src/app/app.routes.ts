89c293847ce409bf37ceaf798182913007368bde
Bart Römgens <bart.romgens@gmail.com>
2025-12-26 17:55:25 +0100

    Remove wet days from UI

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -35,7 +35,6 @@ const CLIMATE_VARIABLE_ROUTES: Record<
     path: 'cloud-cover',
     title: 'Cloud Cover Map',
   },
-  [ClimateVarKey.WET_DAYS]: { path: 'wet-days', title: 'Wet Days Map' },
   [ClimateVarKey.FROST_DAYS]: { path: 'frost-days', title: 'Frost Days Map' },
   [ClimateVarKey.RADIATION]: {
     path: 'radiation',
